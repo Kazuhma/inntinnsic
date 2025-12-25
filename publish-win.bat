4fec4210fa4ce2abb351b8c61a001625bb3bbf1a
Rudolf Dunn <rudolf.d.dunn@gmail.com>
2025-12-25 15:12:39 +0100

    New application icon, releae package automation, fixes

---
+++ publish-win.bat
@@ -0,0 +1,11 @@
+@echo off
+echo Building and publishing Inntinnsic for Windows...
+dotnet publish -c Release -f net8.0-windows10.0.19041.0
+
+echo.
+echo Creating release archive...
+powershell -Command "Compress-Archive -Path bin\Release\net8.0-windows10.0.19041.0\win-x64\publish\* -DestinationPath Inntinnsic-v3.0-win-x64.zip -Force"
+
+echo.
+echo Done! Release package created: Inntinnsic-v3.0-win-x64.zip
+pause
