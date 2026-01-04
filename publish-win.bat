a65220c7c01e7b5a314aa7c9ae86e0f7ca09bbbd
Rudolf Dunn <rudolf.d.dunn@gmail.com>
2026-01-04 22:41:27 +0100

    Created version 3.1 Release Package

--- publish-win.bat
+++ publish-win.bat
@@ -4,8 +4,8 @@ dotnet publish -c Release -f net8.0-windows10.0.19041.0
 
 echo.
 echo Creating release archive...
-powershell -Command "Compress-Archive -Path bin\Release\net8.0-windows10.0.19041.0\win-x64\publish\* -DestinationPath Inntinnsic-v3.0-win-x64.zip -Force"
+powershell -Command "Compress-Archive -Path bin\Release\net8.0-windows10.0.19041.0\win-x64\publish\* -DestinationPath Inntinnsic-v3.1-win-x64.zip -Force"
 
 echo.
-echo Done! Release package created: Inntinnsic-v3.0-win-x64.zip
+echo Done! Release package created: Inntinnsic-v3.1-win-x64.zip
 pause
